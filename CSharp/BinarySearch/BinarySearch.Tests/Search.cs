cf92bf889389bb745555cd43a11924a8c0301c03
Omar <omarjavier15@gmail.com>
2017-11-09 11:37:02 -0400

    Add nested class to indicate context add comment to exapling namming convention

--- CSharp/BinarySearch/BinarySearch.Tests/Search.cs
+++ CSharp/BinarySearch/BinarySearch.Tests/Search.cs
@@ -5,42 +5,52 @@ using Xunit;
 
 namespace BinarySearch.Tests
 {
+  /// <summary>
+  /// The name of each test indicates the number of alelements 
+  /// contained in the list in which the search is tested.
+  /// 
+  /// Each nested class indicates a context and exists in order to
+  /// simplify the name of each test.
+  /// </summary>
   public class Search
   {
-    [Fact]
-    public void _1() =>
-      new List<int> { 1 }
-        .Search(1)
-        .ShouldEqual(0);
+    public class TargetIsContained
+    {
+      [Fact]
+      public void _1() =>
+        new List<int> { 1 }
+          .Search(1)
+          .ShouldEqual(0);
 
-    [Fact]
-    public void _2() =>
-      new List<int> { 10, 15 }
-        .Search(15)
-        .ShouldEqual(1);
+      [Fact]
+      public void _2() =>
+        new List<int> { 10, 15 }
+          .Search(15)
+          .ShouldEqual(1);
 
-    [Fact]
-    public void _3() =>
-      new List<int> { 10, 15, 200 }
-        .Search(15)
-        .ShouldEqual(1);
+      [Fact]
+      public void _3() =>
+        new List<int> { 10, 15, 200 }
+          .Search(15)
+          .ShouldEqual(1);
 
-    [Fact]
-    public void _4_TargetInFirstHalf() =>
-      new List<int> { 10, 15, 200, 201 }
-        .Search(15)
-        .ShouldEqual(1);
+      [Fact]
+      public void _4_TargetInFirstHalf() =>
+        new List<int> { 10, 15, 200, 201 }
+          .Search(15)
+          .ShouldEqual(1);
 
-    [Fact]
-    public void _4_TargetInSecondHalf() =>
-      new List<int> { 10, 15, 200, 201 }
-        .Search(200)
-        .ShouldEqual(2);
+      [Fact]
+      public void _4_TargetInSecondHalf() =>
+        new List<int> { 10, 15, 200, 201 }
+          .Search(200)
+          .ShouldEqual(2);
+    }
 
     public class TargetNotContained
     {
       [Fact]
-      public void _0() => 
+      public void _0() =>
         new List<int>()
           .Search(1)
           .ShouldEqual(-1);
@@ -68,7 +78,7 @@ namespace BinarySearch.Tests
 
       while (min <= max)
       {
-        var guess = (int) Math.Floor((min + max) / 2.0);
+        var guess = (int)Math.Floor((min + max) / 2.0);
 
         if (list[guess] == target)
           return guess;
