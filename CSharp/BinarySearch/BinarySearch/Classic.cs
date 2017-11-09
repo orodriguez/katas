c286e5b1151e3ac50e24504ffe3b5e79bb840b2e
Omar <omarjavier15@gmail.com>
2017-11-09 12:39:05 -0400

    Add classic implementation

--- CSharp/BinarySearch/BinarySearch/Classic.cs
+++ CSharp/BinarySearch/BinarySearch/Classic.cs
@@ -7,7 +7,23 @@ namespace BinarySearch
   {
     public static int Search(IList<int> list, int target)
     {
-      throw new NotImplementedException();
+      var min = 0;
+      var max = list.Count - 1;
+
+      while (min <= max)
+      {
+        var guess = (int)Math.Floor((min + max) / 2.0);
+
+        if (list[guess] == target)
+          return guess;
+
+        if (list[guess] < target)
+          min = guess + 1;
+        else
+          max = guess - 1;
+      }
+
+      return -1;
     }
   }
 }
