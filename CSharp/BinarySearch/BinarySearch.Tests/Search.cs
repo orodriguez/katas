cbe65ba923464e50dc7a634734cef5fc62585157
Omar <omarjavier15@gmail.com>
2017-11-09 10:41:51 -0400

    Search.OneElementList_TargetIsContained Search.OneElementList_TargetNotContined

--- CSharp/BinarySearch/BinarySearch.Tests/Search.cs
+++ CSharp/BinarySearch/BinarySearch.Tests/Search.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Should;
 using Xunit;
 
@@ -7,11 +8,26 @@ namespace BinarySearch.Tests
   public class Search
   {
     [Fact]
-    public void Empty() => new List<int>().Search(1).ShouldEqual(-1);
+    public void EmptyList() => new List<int>().Search(1).ShouldEqual(-1);
+
+    [Fact]
+    public void OneElementList_TargetIsContained() => 
+      new List<int> { 1 }
+        .Search(1)
+        .ShouldEqual(0);
+
+    [Fact]
+    public void OneElementList_TargetNotContined() => 
+      new List<int> { 1 }
+        .Search(2)
+        .ShouldEqual(-1);
   }
 
   public static class ListExtensions
   {
-    public static int Search<T>(this IList<T> list, T target) => -1;
+    public static int Search<T>(this IList<T> list, T target) => 
+      list.Any() && list.First().Equals(target)
+        ? 0 
+        : -1;
   }
 }
