ef3cb71ede93ecd9c4f041fafa8cff653053dcc4
Omar <omarjavier15@gmail.com>
2017-11-09 10:33:34 -0400

    Search: Empty List

---
+++ CSharp/BinarySearch/BinarySearch.Tests/Search.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Should;
+using Xunit;
+
+namespace BinarySearch.Tests
+{
+    public class Search
+    {
+      [Fact]
+      public void Empty() => new List<int>().Search(1).ShouldEqual(-1);
+    }
+
+  public static class ListExtensions
+  {
+    public static int Search<T>(this IList<T> list, T target) => -1;
+  }
+}
