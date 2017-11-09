bafe18523e455984215b1afb8e0160c03e15ecd4
Omar <omarjavier15@gmail.com>
2017-11-09 12:10:44 -0400

    Introduce struct IndexRange

--- CSharp/BinarySearch/BinarySearch/BinarySearch.cs
+++ CSharp/BinarySearch/BinarySearch/BinarySearch.cs
@@ -7,23 +7,40 @@ namespace BinarySearch
   {
     public static int Search(IList<int> list, int target)
     {
-      var min = 0;
-      var max = list.Count - 1;
+      var range = new IndexRange(from: 0, to: list.Count - 1);
 
-      while (min <= max)
+      while (range.Size > 0)
       {
-        var guess = (int) Math.Floor((min + max) / 2.0);
+        var guess = range.Guess();
 
         if (list[guess] == target)
           return guess;
 
-        if (list[guess] < target)
-          min = guess + 1;
-        else
-          max = guess - 1;
+        range = list[guess] < target ? range.After(guess) : range.Before(guess);
       }
 
       return -1;
+    }
+
+    private struct IndexRange
+    {
+      public IndexRange(int from, int to)
+      {
+        From = from;
+        To = to;
+      }
+
+      public int Size => To - From + 1;
+
+      public int Guess() => (int) Math.Floor((From + To) / 2.0);
+
+      public IndexRange After(int index) => new IndexRange(index + 1, this.To);
+
+      public IndexRange Before(int index) => new IndexRange(this.From, index - 1);
+
+      private int To { get; }
+
+      private int From { get; }
     }
   }
 }
