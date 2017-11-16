ffd23ae8973fac59a7b0933bb38b0bd25ee36f02
Omar <omarjavier15@gmail.com>
2017-11-16 15:15:27 -0400

    User tuples in Recursive

--- CSharp/BinarySearch/BinarySearch/Recursive.cs
+++ CSharp/BinarySearch/BinarySearch/Recursive.cs
@@ -1,23 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace BinarySearch
 {
   public class Recursive
   {
-    public static int Search(IList<int> list, int target) => Search(list, target, new IndexRange(list));
+    public static int Search(IList<int> list, int target) => Search(list, target, (0, list.Count - 1));
 
-    private static int Search(IList<int> list, int target, IndexRange range)
+    private static int Search(IList<int> list, int target, (int From, int To) range)
     {
       if (range.From > range.To)
         return -1;
 
-      var guess = range.MiddleIndex();
+      var guess = (int)Math.Floor((range.From + range.To) / 2.0); ;
 
       if (list[guess] == target)
         return guess;
 
       return Search(list, target, 
-        list[guess] < target ? range.After(guess) : range.Before(guess));
+        list[guess] < target 
+          ? (From: guess + 1, To: range.To) 
+          : (From: range.From, To: guess - 1));
     }
   }
 }
