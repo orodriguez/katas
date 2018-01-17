f10d0da15d93fa3d8165a875a55a0bdbc207be84
Omar <omarjavier15@gmail.com>
2018-01-17 14:30:10 -0400

    Formatting

--- javascript/string-transformations/app/transform.js
+++ javascript/string-transformations/app/transform.js
@@ -1,22 +1,21 @@
 const transform = (transformationsStr, str) => {
   const parseResult = parseTransformations(transformationsStr);
-  
+
   if (!parseResult.valid())
     return parseResult.error();
 
   return composed(parseResult.transformations())(str);
 }
 
-const toPascalCase = str => 
-  str.split(/(\s)/g) 
-  .map(token => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase());
+const toPascalCase = str =>
+  str.split(/(\s)/g)
+    .map(token => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase());
 
 const toCamelCase = str => {
   let found = false;
   return toPascalCase(str)
     .map(token => {
-      if (!found && token.match(/\w+/))
-      {
+      if (!found && token.match(/\w+/)) {
         found = true;
         return token.toLowerCase();
       }
@@ -25,21 +24,21 @@ const toCamelCase = str => {
     });
 };
 
-const toSnakeCase = str => 
-str.split(/[,!@#$%^&*()\s]/).join('_');
+const toSnakeCase = str =>
+  str.split(/[,!@#$%^&*()\s]/).join('_');
 
 const pack = str => str.split(/[\t\n\d\r\s]/).join('');
 
 const handlers = {
-'': str => str,
-'Lowercase': str => str.toLowerCase(),
-'Uppercase': str => str.toUpperCase(),
-'Trim-Start': str => str.trimLeft(),
-'Trim-End': str => str.trimRight(),
-'PascalCase': str => toPascalCase(str).join(''),
-'CamelCase': str => toCamelCase(str).join(''),
-'SnakeCase': str => toSnakeCase(str),
-'Pack': pack
+  '': str => str,
+  'Lowercase': str => str.toLowerCase(),
+  'Uppercase': str => str.toUpperCase(),
+  'Trim-Start': str => str.trimLeft(),
+  'Trim-End': str => str.trimRight(),
+  'PascalCase': str => toPascalCase(str).join(''),
+  'CamelCase': str => toCamelCase(str).join(''),
+  'SnakeCase': str => toSnakeCase(str),
+  'Pack': pack
 };
 
 const parseTransformations = transformations => {
@@ -51,8 +50,8 @@ const parseTransformations = transformations => {
 
 const parseTransformation = transformationStr => {
   const transformation = handlers[transformationStr];
-  return transformation 
-    ? { valid: true, token: transformationStr, handler: transformation } 
+  return transformation
+    ? { valid: true, token: transformationStr, handler: transformation }
     : { valid: false, token: transformationStr };
 };
 
@@ -63,14 +62,13 @@ const composed = transformations =>
     .reduce((composited, current) => compose(composited, current));
 
 const createParseResult = parsedTransformations => ({
-  
   valid: () => !parsedTransformations.some(r => !r.valid),
-  
+
   error: () => {
     const invalid = parsedTransformations.find(r => !r.valid);
-    return `Invalid Transformation "${invalid.token}"`
+    return `Invalid Transformation "${invalid.token}"`;
   },
-  
+
   transformations: () => parsedTransformations.map(t => t.handler)
 });
 
