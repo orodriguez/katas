fc7e190df7911ad9a6aadbd67c39a610acd3b418
Omar <omarjavier15@gmail.com>
2018-01-18 19:17:15 -0400

    Organize tests as theory

---
+++ CSharp/TextStatistics/TextStatistics.Tests/Text/AnalizeTest.cs
@@ -0,0 +1,15 @@
+﻿using Should;
+using Xunit;
+
+namespace TextStatistics.Tests.Text
+{
+  public class AnalizeTest
+  {
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("Hello", 1)]
+    [InlineData(" Hello", 1)]
+    public void TotalWordCount(string text, int expectedCount) => 
+      text.Analize().TotalWords.ShouldEqual(expectedCount);
+  }
+}
