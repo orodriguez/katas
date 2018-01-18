968fad9d029733052f400166eb561bab06f75be3
Omar <omarjavier15@gmail.com>
2018-01-18 18:58:54 -0400

    Xunit installed

--- CSharp/TextStatistics/TextStatistics.Tests/Class1.cs
+++ CSharp/TextStatistics/TextStatistics.Tests/Class1.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Xunit;
 
 namespace TextStatistics.Tests
 {
-    public class Class1
+  public class Class1
+  {
+    [Fact]
+    public void Sample()
     {
+      Assert.True(true);
     }
+  }
 }
