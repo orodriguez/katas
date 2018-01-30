6dd89f7ae1e5f8fd9cca48e56fdfec90dbed2d98
Omar <omarjavier15@gmail.com>
2018-01-30 19:08:25 -0400

    Text statistics v3

--- CSharp/TextStatistics/TextStatistics.Core/Text.cs
+++ CSharp/TextStatistics/TextStatistics.Core/Text.cs
@@ -5,21 +5,24 @@ namespace TextStatistics.Core
 {
   public class Text
   {
-    public Text(string str) => Str = str;
+    public Text(string str) => 
+      Str = str;
 
-    public (string, int)[] CharFreq() => CountMatches(@"\w");
+    public (string, int)[] CharFreq() => 
+      CountMatches(@"\w");
 
-    public (string, int)[] WordFreq() => CountMatches(@"\w+");
-
-    private (string, int)[] CountMatches(string pattern) => 
-      Regex.Matches(Str, pattern)
-        .GroupBy(m => m.Value)
-        .Select(matches => (matches.Key, matches.Count()))
-        .ToArray();
+    public (string, int)[] WordFreq() =>
+      CountMatches(@"\w+");
 
     public double CharWordRation() => 
       (double)CharFreq().Length / WordFreq().Length;
 
     private string Str { get; }
+
+    private (string, int)[] CountMatches(string pattern) => 
+      Regex.Matches(Str, pattern)
+        .GroupBy(match => match.Value)
+        .Select(group => (group.Key, group.Count()))
+        .ToArray();
   }
 }
