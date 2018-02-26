97f623f9fdcd89bc18b03cf58150b00212d897af
Omar Rodriguez <omarjavier15@gmail.com>
2018-02-26 17:49:20 -0400

    Organize tests like cucumber scenario outline

--- ruby/doublets/spec/doublets_spec.rb
+++ ruby/doublets/spec/doublets_spec.rb
@@ -1,17 +1,42 @@
 module Doublets
   def self.find(dic, word, to)
-    []
+    # Implementation
   end
 end
 
 describe Doublets do
-  subject { Doublets.find dictionary, word, to }
+  examples = [
+    {from: 'door', to: 'lock', expected: ['door', 'boor', 'book', 'look', 'lock']},
+    {from: 'bank', to: 'loan', expected: ['bank', 'bonk', 'book', 'look', 'loon', 'loan']},
+    {from: 'wheat', to: 'bread', expected: ['wheat', 'cheat', 'cheap', 'cheep', 'creep', 'creed', 'breed', 'bread']},
+  ]
 
-  context 'Empty dictionary' do
-    let(:dictionary) { [] }
-    let(:word) { '' }
-    let(:to) { '' }
+  examples.each do |example|
+    context "when #{example.inspect}" do
+      subject { Doublets.find dictionary, example[:word], example[:to] }
 
-    it { is_expected.to eq [] }
+      it { is_expected.to eq example[:expected] }
+    end
+  end
+
+  let :dictionary do
+    [
+      'door',
+      'boor',
+      'book',
+      'look',
+      'lock',
+      'bank',
+      'bonk',
+      'book',
+      'wheat',
+      'cheat',
+      'cheap',
+      'cheep',
+      'creep',
+      'creed',
+      'breed',
+      'bread',
+    ]
   end
 end
